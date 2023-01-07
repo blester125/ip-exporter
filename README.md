a696e5379f1ba8b19c5b6fe156e55e12b5db858c
Brian Lester <blester125@gmail.com>
2023-01-07 16:12:30 -0500

    ups

---
+++ README.md
@@ -0,0 +1,3 @@
+# ip-exporter
+
+A small script using the prometheus client to log your public IP address.
--- ip-exporter.py
+++ ip-exporter.py
@@ -5,7 +5,7 @@ import logging
 import os
 import requests
 import time
-from prometheus_client import start_http_server, Info
+from prometheus_client import start_http_server, Guage
 
 
 parser = argparse.ArgumentParser(description="Public IP as Prometheus metrics.")
@@ -13,7 +13,7 @@ parser.add_argument("--port", type=int, default=9191)
 parser.add_argument("--delay", type=int, default=60 * 5, help="time in seconds")
 args = parser.parse_args()
 
-IP = Info("public_ip", "The public IP address of the host.")
+IP = Gauge("public_ip", "The public IP address of the host.", ['ip', 'host'])
 URL = "https://api.ipify.org/?format=json"
 
 logging.basicConfig(format="%(asctime)s %(message)s", level=logging.INFO)
@@ -23,7 +23,7 @@ def get_public_ip():
     fqn = os.uname()[1]
     public_ip = requests.get(URL).json()['ip']
     logging.info("ip: %s host: %s", public_ip, fqn)
-    IP.info({"ip": public_ip, "host": fqn})
+    IP.labels(ip=public_ip, host=fqn).set(1)
 
 
 if __name__ == "__main__":
